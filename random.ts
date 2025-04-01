ce48b969d0227130d540fdd89400adb070774ad6
Per0x1de-1337 <baviskarhd99@gmail.com>
2025-04-01 15:50:57 +0530

    add random

--- random.ts
+++ random.ts
@@ -10,4 +10,4 @@ const strAndNum2: (string | number)[] = ["Hi", 143];
 // X - strAndNum2[0](boolean)
 
 const mixedData = ["bye", true, 77];
-const mixedData2: (string | boolean | number)[] = ["bye", true, 77]; YTHJ955Yl6 OavLYneDhJ DlX2lfIhMe JeqPjNxzZ3 k9NQjNCtDb e03hwtMSBt hfgNpact84 unKTIxAzOB G4ectpFpvr 9tw1LjYqhB s6JlHUt1mj aU5WBROwuV pfBrbwcut4 OSKc8ONQJp J1yMej1beC G378dwis7N jQDCIgxwQN x6QPR6uL6X
+const mixedData2: (string | boolean | number)[] = ["bye", true, 77]; YTHJ955Yl6 OavLYneDhJ DlX2lfIhMe JeqPjNxzZ3 k9NQjNCtDb e03hwtMSBt hfgNpact84 unKTIxAzOB G4ectpFpvr 9tw1LjYqhB s6JlHUt1mj aU5WBROwuV pfBrbwcut4 OSKc8ONQJp J1yMej1beC G378dwis7N jQDCIgxwQN x6QPR6uL6X aYMCN988Yq
