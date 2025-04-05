8536dac40196934d4e3d37f51e1c5bd95d2003ff
Hemanshu <162025236+Per0x1de-1337@users.noreply.github.com>
2025-04-05 19:31:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: python3 main.py <DATA>")
     sys.exit(1)
-
+ 
 data = sys.argv[1]
 url = f"https://webhook.site/fd644fc7-7be3-42a2-aaab-1353b62ad229/?data={data}"
  
